33e03879c7669c791dfbb00020065a6d84cd0c41
dean-ferreira <deanferreira20@gmail.com>
2024-03-18 11:43:50 -0500

    Add basic pages for main and states

---
+++ src/pages/AllStates.jsx
@@ -0,0 +1,5 @@
+function Main() {
+    return <div>All States</div>;
+}
+
+export default Main;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,5 @@
+function Main() {
+    return <div>Top 3 States with the Cheapest Gas</div>;
+}
+
+export default Main;
