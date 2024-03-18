a3b336dc697abfec5fb24a464dcf06cff65818bf
dean-ferreira <deanferreira20@gmail.com>
2024-03-18 15:30:29 -0500

    Add request for all states

--- src/pages/AllStates.jsx
+++ src/pages/AllStates.jsx
@@ -1,5 +1,53 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function Main() {
-    return <div>All States</div>;
+    const [states, setStates] = useState([]);
+    let apiKey = import.meta.env.VITE_API_KEY;
+    const options = {
+        method: 'GET',
+        url: 'https://api.collectapi.com/gasPrice/allUsaPrice',
+        headers: {
+            'content-type': 'application/json',
+            authorization: `apikey ${apiKey}`,
+        },
+    };
+
+    async function getStates() {
+        try {
+            const response = await axios(options);
+            setStates(response.data.result);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    useEffect(() => {
+        getStates();
+    }, []);
+
+    const loaded = () => {
+        return (
+            <div>
+                All States
+                {states.map((state, index) => (
+                    <div key={index}>
+                        <h2>{state.name}</h2>
+                        <h3>{state.gasoline}</h3>
+                        <h3>{state.midGrade}</h3>
+                        <h3>{state.premium}</h3>
+                        <h3>{state.diesel}</h3>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    const loading = () => {
+        return <h1>Loading...</h1>;
+    };
+
+    return states ? loaded() : loading();
 }
 
 export default Main;
